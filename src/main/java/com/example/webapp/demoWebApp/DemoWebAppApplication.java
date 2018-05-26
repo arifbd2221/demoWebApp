2ff80c1b22f2e564a335224c793ec41359553ec5
arifbd2221 <mohidulhoque216@gmail.com>
2018-05-26 22:28:34 +0600

    Initial commit

---
+++ src/main/java/com/example/webapp/demoWebApp/DemoWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.webapp.demoWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebAppApplication.class, args);
+	}
+}
